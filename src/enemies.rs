c6756cd4a28431fa2e9858ce55379c6c7c404729
s210951 <s210951@th-ab.de>
2026-06-13 18:46:20 +0200

    Project Structure

---
+++ src/enemies.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+pub struct EnemiePlugin;
+
+impl Plugin for EnemiePlugin {
+    fn build(&self, app: &mut App) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod enemies;
+mod movement;
+use bevy::{ecs::system::command, prelude::*};
+use enemies::EnemiePlugin;
+use movement::MovementPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins((DefaultPlugins, MovementPlugin, EnemiePlugin));
+    app.add_systems(Startup, setup);
+    app.run();
 }
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Mesh2d(meshes.add(Circle::new(50.0))),
+        MeshMaterial2d(materials.add(Color::hsl(180.0, 1.0, 1.0))),
+        Transform::IDENTITY,
+        Player,
+    ));
+}
+
+#[derive(Component)]
+struct Player;
